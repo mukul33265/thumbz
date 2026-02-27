0282e52a1176889a29a996416ef1903f6a0ae830
mukul33265 <mukulkumar7707@gmail.com>
2026-02-27 11:08:35 +0530

    ds

--- server/middleware.js/auth.js
+++ server/middleware.js/auth.js
@@ -1,11 +1,11 @@
-const protect = (req, res, next) => {
-    if (!req.session || !req.session.isLoggedIn || !req.session.userId) {
+const protect = async (req,res,next) => {
+    const {isLoggedIn,userId} = req.session ;
+    
+    if(!isLoggedIn || !userId) {
         return res.status(401).json({
-            message: "You are not logged in"
+            message : 'You are not logged in'
         });
     }
-
     next();
-};
-
-module.exports = { protect };
+}
+module.exports = {protect} ;
